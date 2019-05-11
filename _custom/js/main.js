686f602dc8de95a339c7bd99f97ab15edbe4d031
Joscha Jaeger <joscha.jaeger@gmail.com>
2019-05-11 15:11:30 +0100

    fix accidental play

--- _custom/js/main.js
+++ _custom/js/main.js
@@ -877,7 +877,7 @@ function activateLayer(layerName, videoID) {
 						$(window).resize();
 						updateMuted();
 						renderPlayCircleLinks();
-						FieldTrip.play();
+						if (currentLayer == 'hypervideo') FieldTrip.play();
 						$('.hypervideo .video').stop(true, false).animate({
 							volume: 1
 						}, 2000);
